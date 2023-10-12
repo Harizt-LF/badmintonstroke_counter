0c732240548588bb57f907d22885f0b5fda35188
Harizt-LF <hariztfajree@gmail.com>
2023-10-12 22:34:12 +0700

    fix : search for service uid

--- lib/views/counter_screen.dart
+++ lib/views/counter_screen.dart
@@ -14,17 +14,52 @@ class _CounterScreenState extends State<CounterScreen> {
   List<BluetoothService> _service = [];
   bool _isDiscoveringServices = false;
   bool _isConnecting = false;
+  bool _isConnected = false;
+  bool _isEmpty = true;
   String targetDevice = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
 
   Future discoverServices() async {
     _service = await widget.device.discoverServices();
-    print(_service);
+    setState(() {
+      _isEmpty = false;
+    });
+  }
+
+  Future onConnect() async {
+    await widget.device.connect(timeout: Duration(seconds: 5));
+    setState(() {
+      _isConnected = true;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    discoverServices();
     return Scaffold(
-        body: Center(child: Text(widget.device.platformName.toString())));
+      appBar: AppBar(
+        leading: _isConnected ? Text('Terhubung') : Text('Tidak Terhubung'),
+      ),
+      body: Center(
+        child: Column(children: [
+          FloatingActionButton(
+              onPressed: onConnect, child: Icon(Icons.bluetooth_connected)),
+          TextButton(
+            onPressed: () {
+              discoverServices();
+            },
+            child: Text(
+              widget.device.platformName.toString(),
+            ),
+          ),
+          Text(_isEmpty ? 'Gada data' : 'Ada data ${_service.length}'),
+          Text(_isEmpty ? 'Maklo' : 'data ${_service[2].serviceUuid}'),
+          // ListView.builder(
+          //   itemCount: _service.length,
+          //   itemBuilder: (BuildContext context, index) {
+          //     return Text('Ini datanya bang : ${_service[index].serviceUuid}');
+          //   },
+          // )
+        ]),
+      ),
+    );
   }
 }
