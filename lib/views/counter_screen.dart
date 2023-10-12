3eadf211d43e5b80cd73165f9220b1990887f35f
Harizt-LF <hariztfajree@gmail.com>
2023-10-13 00:58:56 +0700

    feat: read data from ble

--- lib/views/counter_screen.dart
+++ lib/views/counter_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
@@ -12,11 +15,32 @@ class CounterScreen extends StatefulWidget {
 
 class _CounterScreenState extends State<CounterScreen> {
   List<BluetoothService> _service = [];
+  late StreamSubscription<List<int>> _lastValueSubscription;
+  List<int> _value = [];
+  var nilai = '';
   bool _isDiscoveringServices = false;
   bool _isConnecting = false;
   bool _isConnected = false;
   bool _isEmpty = true;
+  bool _dataIsEmpty = true;
+  var _actualValue = '';
   String targetDevice = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _lastValueSubscription =
+  //       _service[2].characteristics[0].lastValueStream.listen((value) {
+  //     _value = value;
+  //     setState(() {});
+  //   });
+  // }
+
+  // @override
+  // void dispose() {
+  //   _lastValueSubscription.cancel();
+  //   super.dispose();
+  // }
 
   Future discoverServices() async {
     _service = await widget.device.discoverServices();
@@ -29,6 +53,15 @@ class _CounterScreenState extends State<CounterScreen> {
     await widget.device.connect(timeout: Duration(seconds: 5));
     setState(() {
       _isConnected = true;
+    });
+  }
+
+  Future read() async {
+    final utf8Decoder = utf8.decoder;
+    _value = await _service[2].characteristics[0].read();
+    _actualValue = utf8Decoder.convert(_value);
+    setState(() {
+      _dataIsEmpty = false;
     });
   }
 
@@ -52,6 +85,13 @@ class _CounterScreenState extends State<CounterScreen> {
           ),
           Text(_isEmpty ? 'Gada data' : 'Ada data ${_service.length}'),
           Text(_isEmpty ? 'Maklo' : 'data ${_service[2].serviceUuid}'),
+          Text(_isEmpty
+              ? ''
+              : 'banyak characteristic : ${_service[2].characteristics.length}'),
+          TextButton(onPressed: read, child: Text('Cari value')),
+          Text(_dataIsEmpty == false ? 'data ${_actualValue}' : 'No data')
+          //TextButton(onPressed: read, child: Text('Read Data : ${_value.}')),
+          // Text(_dataIsEmpty ? 'Data kosong ' : 'data : ${nilai.toString()}')
           // ListView.builder(
           //   itemCount: _service.length,
           //   itemBuilder: (BuildContext context, index) {
